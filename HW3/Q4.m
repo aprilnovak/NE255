140622de0627d47690049e4d079d2d0198efbaa3
aprilnovak <novak@berkeley.edu>
2016-10-16 14:54:14 -0700

    Finished part a, Q4.

--- HW3/Q4.m
+++ HW3/Q4.m
@@ -1,11 +1,12 @@
 % Question 4, HW 3
 clear all
 
-j = 1;      % subplot number
+j = 1;              % subplot number
+real_plot = 1;      % 1 = plot real components, 0 = plot imaginary
 
 for l = [0, 1, 2];
     for m = -l:l
-        spacing = 0.1;
+        spacing = 0.15;
         theta = meshgrid(0:spacing:pi);
         mu = cos(theta);
         phi = meshgrid(linspace(0, 2*pi, length(theta)));
@@ -40,20 +41,28 @@ for l = [0, 1, 2];
         prefactor2 = sqrt(4 * pi * factorial(l + m));
         Y_lm = ((-1).^ m) .* (prefactor ./ prefactor2) .* P_lm .* exp(1i .* m .* Phi);
 
-        % plot the real component
-        % mesh(Theta, Phi, real(Y_lm));
-        % zlabel('Real Component of Ylm')
-        % xlabel('\theta')
-        % ylabel('\phi')
-
-        % plot the imaginary component
-        subplot(3, 3, j)
-        surf(Theta, Phi, imag(Y_lm));
-        title(sprintf('l = %i, m = %i', l, m))
-        zlabel('Imaginary Component of Ylm')
-        xlabel('Theta (\theta)')
-        ylabel('Phi (\phi)')
+        if (real_plot == 1)
+            % plot the real component
+            subplot(3, 3, j)
+            surf(Theta, Phi, real(Y_lm));
+            title(sprintf('l = %i, m = %i', l, m))
+            zlabel('Re(Ylm)')
+            xlabel('Theta (\theta)')
+            ylabel('Phi (\phi)')
+            plot_title = 'Real_Ylm';
+        else
+            % plot the imaginary component
+            subplot(3, 3, j)
+            surf(Theta, Phi, imag(Y_lm));
+            title(sprintf('l = %i, m = %i', l, m))
+            zlabel('Im(Ylm)')
+            xlabel('Theta (\theta)')
+            ylabel('Phi (\phi)')
+            plot_title = 'Imag_Ylm';
+        end
 
         j = j + 1;
     end
 end
+
+saveas(gcf, plot_title, 'pdf')
