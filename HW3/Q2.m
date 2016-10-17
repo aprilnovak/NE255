ddd4b8ff614a094f95ce4937f967965f889f6567
aprilnovak <novak@berkeley.edu>
2016-10-16 22:52:49 -0700

    Added additional functions for integration.

--- HW3/Q2.m
+++ HW3/Q2.m
@@ -1,14 +1,16 @@
 % QUESTION 2 - NE 255 hw 2
 clear all
 
-N = 6;                                          % quadrature order
+N = 8;                                          % quadrature order
 syms xe eta mu                                  % components of \Omega
 
 % Parts A and B
 %f(xe, eta, mu) = sqrt(xe^2 + eta^2 + mu^2);    % function to integrate
 
 % Part C (uncomment to see the result)
-f(xe, eta, mu) = mu^2;
+%f(xe, eta, mu) = mu;
+%f(xe, eta, mu) = mu^2;
+f(xe, eta, mu) = cos(mu);
 
 switch N
     case 4
@@ -105,11 +107,11 @@ integral = 0;
 j = 1;
 for i = 1:(N*(N+2))
     integral = integral +  wt(j) * f(valid_full(i,1), valid_full(i,2), valid_full(i,3));
-    if j == length(valid)
+    if (mod(i, 8) == 0)
         j = j + 1;
     end
 end
 
 % multiply result by pi/2 to scale
 integral = eval(integral) * pi/2;
-disp(sprintf('S-%i quadrature gives an integral of: %.4f', N, integral));
+disp(sprintf('S-%i quadrature gives an integral of: %.8f', N, integral));
