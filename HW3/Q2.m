69a3377afc0ff85ca98beadf1abd451df67570e5
aprilnovak <novak@berkeley.edu>
2016-10-16 21:46:23 -0700

    Performed the integration.

--- HW3/Q2.m
+++ HW3/Q2.m
@@ -1,16 +1,28 @@
 % QUESTION 2 - NE 255 hw 2
 clear all
-N = 6;          % quadrature order
+
+N = 8;                                          % quadrature order
+syms xe eta mu                                  % components of \Omega
+f(xe, eta, mu) = sqrt(xe^2 + eta^2 + mu^2);     % function to integrate
 
 switch N
     case 4
         mu_n = [0.3500212, 0.8688903];
+        wt1 = 1/3;
+        wt = [wt1, wt1, wt1];
     case 6
         mu_n = [0.2666355, 0.6815076, 0.9261808];
+        wt1 = 0.1761263;
+        wt2 = 0.1572071;
+        wt = [wt1, wt2, wt1, wt2, wt2, wt1];
     case 8
         mu_n = [0.2182179, 0.5773503, 0.7867958, 0.9511897];
+        wt1 = 0.1209877;
+        wt2 = 0.0907407;
+        wt3 = 0.0925926;
+        wt = [wt1, wt2, wt2, wt1, wt2, wt3, wt2, wt2, wt2, wt1];
     otherwise
-        disp('Incorrect quadrature order.');
+        disp('Non-supported quadrature order.');
 end
 
 % permute to find the total number of possible quadrature points
@@ -51,3 +63,10 @@ for i = 1:total_possible
         k = k + 1;
     end
 end
+
+% perform the integration
+integral = 0;
+for i = 1:(N*(N+2)/8)
+    integral = integral +  wt(i) * f(valid(i,1), valid(i,2), valid(i,3));
+end
+
