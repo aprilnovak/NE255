e55395f53e25f2bf3cc17e5e10a4250662c3a420
aprilnovak <novak@berkeley.edu>
2016-10-16 21:29:00 -0700

    Finished permutation for arbitrary quadrature order.

--- HW3/Q2.m
+++ HW3/Q2.m
@@ -1,14 +1,17 @@
 % QUESTION 2 - NE 255 hw 2
 clear all
-N = 4;          % quadrature order
+N = 6;          % quadrature order
 
 switch N
     case 4
         mu_n = [0.3500212, 0.8688903];
+    case 6
+        mu_n = [0.2666355, 0.6815076, 0.9261808];
+    case 8
+        mu_n = [0.2182179, 0.5773503, 0.7867958, 0.9511897];
     otherwise
         disp('Incorrect quadrature order.');
 end
-mu_n = [1 2 3];
 
 % permute to find the total number of possible quadrature points
 i = 1;              % index for first
@@ -35,5 +38,16 @@ for row = 1:total_possible
                 i = 1;
             end
         end
+    end
+end
+
+valid = zeros(N*(N+2)/8, 3);
+k = 1;
+% check to see which quadrature combinations are valid (2-norm equals 1)
+for i = 1:total_possible
+    norm = sqrt(total(i,1)^2 + total(i,2)^2 + total(i,3)^2);
+    if (abs(norm - 1) < 1e-2)
+        valid(k,:) = total(i,:);
+        k = k + 1;
     end
 end
