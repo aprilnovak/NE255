21c4329e4d0da7cd08f664d5b1089ebb0182d5ab
aprilnovak <novak@berkeley.edu>
2016-11-04 13:39:59 -0700

    Finished part c, problem 3.

--- HW4/Q3partCD.m
+++ HW4/Q3partCD.m
@@ -9,16 +9,16 @@ mu = 0.2;
 mu07 = 0.7;
 Et = 1.0;
 Es = 0.5;
-tolerance = 0.05;
+tolerance = 0.01;
 norm = 1; % intial arbitrary value
 num_iterations = 0;
 max_iterations = 100;
+plot_num = 5;
 
-for alpha = [0.0]
-    plot_num = 1;
+for alpha = [0.5]
 
-    for h = [0.08]
-
+    for h = [0.4]
+        
         mesh = 0:h:2;
         elem_length = mesh(2);
         coordinates = 0:(elem_length / 2):2;
@@ -39,7 +39,6 @@ for alpha = [0.0]
             psi_n = zeros(1, length(coordinates));
             psi07 = zeros(1, length(coordinates));
             psi_n07 = zeros(1, length(coordinates));
-            phi = zeros(1, length(coordinates(2:2:end)));
 
             psi(1) = 2.0; % incoming flux boundary condition
 
@@ -78,62 +77,39 @@ for alpha = [0.0]
                 i = i - 2;
             end
             
-
-            % plot the cell-centered values of angular flux
-            % plot(coordinates(2:2:end), psi(2:2:end), '*', coordinates(2:2:end), psi_n(2:2:end), 'r*')
-            % legend('Flux for Positive \mu', 'Flux for Negative \mu')
-            % xlabel('Problem Domain', 'FontSize', fontsize)
-            % ylabel('Cell-Centered Angular Flux', 'FontSize', fontsize)
-            % saveas(gcf, 'AngularFluxh08', 'jpeg')
-            % close all
-
-
-            % apply "quadrature rule" to get scalar flux
-            %phi = psi(2:2:end) + psi_n(2:2:end); % equal weights of 1
-
-            %plot_num = plot_num + 1;
-            
-            phi_old = phi;
-            % compute scalar flux
-            phi = 0.5 .* (psi(2:2:end) + psi_n(2:2:end) + psi07(2:2:end) + psi_n07(2:2:end));
-            
-            
-            % update the scattering source
+            % update the scattering source, and add the external source
             q_new = zeros(1, length(psi));
-            q_new = Es .* 0.5 .* (psi + psi_n + psi07 + psi_n07);
+            q_new = Es .* 0.5 .* (psi + psi_n + psi07 + psi_n07) + 1.0.*coordinates;
 
             % test for convergence
             norm = 0;
-            norm2 = 0;
             for u = 1:length(q_new)
                 norm = norm + (q_new(u) - q(u)).^2;
             end
             norm = sqrt(norm);
             
-            for u = 1:length(phi)
-                norm2 = norm2 + (phi_old(u) - phi(u)).^2;
-            end
-            norm2 = sqrt(norm2);
-            
             q = q_new;
+            
             num_iterations = num_iterations + 1;
             if num_iterations >= max_iterations
                 disp('Maximum number of iterations reached!')
             end
-
-
+            
         end
-    end
-    
+        
+        % compute scalar flux
+        phi = 0.5 .* (psi(2:2:end) + psi_n(2:2:end) + psi07(2:2:end) + psi_n07(2:2:end));
+        
+        % plot the scalar flux
+        subplot(3, 2, plot_num)
+        plot(coordinates(2:2:end), phi, '*')
+        hold on
+        grid on
+        legend(sprintf('h = %.2f', h))
+        xlabel('Problem Domain', 'FontSize', fontsize-2)
+        ylabel('Scalar Flux', 'FontSize', fontsize-2)
+    end 
 end
 
-% plot the scalar flux
-% subplot(3, 2, plot_num)
-plot(coordinates(2:2:end), phi, '*')
-hold on
-grid on
-legend(sprintf('h = %.2f', h))
-xlabel('Problem Domain', 'FontSize', fontsize-2)
-ylabel('Scalar Flux', 'FontSize', fontsize-2)
-%saveas(gcf, sprintf('ScalarFlux%i', 100*alpha), 'jpeg')
+%saveas(gcf, 'ScalarFlux_partC', 'jpeg')
 %close all
