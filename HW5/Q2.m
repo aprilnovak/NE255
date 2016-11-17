da97f69e20696b7acfb6f20e22840d0cb8288a3d
aprilnovak <novak@berkeley.edu>
2016-11-17 15:11:32 -0800

    added external sol sources for each group.

--- HW5/Q2.m
+++ HW5/Q2.m
@@ -26,11 +26,17 @@ mesh = 0:h:2;
 elem_length = mesh(2);
 coordinates = 0:(elem_length / 2):2;
 
-% initial guess for the scattering source for __each__ group. After each 
+% external sources
+qe(1,:) = 1.5 .* ones(1, length(coordinates));
+qe(2,:) = 0.0 .* ones(1, length(coordinates));
+qe(3,:) = 0.2 .* ones(1, length(coordinates));
+
+% initial guess for the scattering source for __each__ group. After each 1
 % within-group calculation, this source will be specialized depending
 % on the energy group.
 
 for g = 1:G
+        % initial guess for the SCATTERING source
         q = 1.0 .* coordinates;
         norm = 1;
         % each within-group calculation finds the angular flux for that group
@@ -49,18 +55,20 @@ for g = 1:G
             psi07 = zeros(1, length(coordinates));
             psi_n07 = zeros(1, length(coordinates));
 
-            psi02(1) = 2.0; % incoming flux boundary condition
-            psi05(1) = 2.0; % incoming flux boundary condition
-            psi07(1) = 2.0; % incoming flux boundary condition
+            if g == 1
+                psi02(1) = 0.5/3; % incoming flux boundary condition
+                psi05(1) = 0.5/3; % incoming flux boundary condition
+                psi07(1) = 0.5/3; % incoming flux boundary condition
+            end
 
             % positive sweep
             i = 1;
 
             while i <= (length(coordinates) - 2)
                 % compute cell-centered value
-                psi02(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu02) * psi02(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu02) / elem_length);
-                psi05(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu05) * psi05(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu05) / elem_length);
-                psi07(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu07) * psi07(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu07) / elem_length);
+                psi02(i + 1) = (q(i+1) + qe(g, i+1) + (2 / (1 + alpha)) * (abs(mu02) * psi02(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu02) / elem_length);
+                psi05(i + 1) = (q(i+1) + qe(g, i+1) + (2 / (1 + alpha)) * (abs(mu05) * psi05(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu05) / elem_length);
+                psi07(i + 1) = (q(i+1) + qe(g, i+1) + (2 / (1 + alpha)) * (abs(mu07) * psi07(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu07) / elem_length);
 
                 % compute out-going value
                 psi02(i + 2) = (2 / (1 + alpha)) * psi02(i + 1) - ((1 - alpha)/(1 + alpha)) * psi02(i);
@@ -80,9 +88,9 @@ for g = 1:G
             while i > 2
 
                 % compute the cell-centered flux
-                psi_n02(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu02 * psi_n02(i) / elem_length)) / (Et(g) + 2 * mu02 / (elem_length * (1 - alpha)));
-                psi_n05(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu05 * psi_n05(i) / elem_length)) / (Et(g) + 2 * mu05 / (elem_length * (1 - alpha)));
-                psi_n07(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu07 * psi_n07(i) / elem_length)) / (Et(g) + 2 * mu07 / (elem_length * (1 - alpha)));
+                psi_n02(i - 1) = (q(i-1) + qe(g, i-1) + (2/(1 - alpha)) * (mu02 * psi_n02(i) / elem_length)) / (Et(g) + 2 * mu02 / (elem_length * (1 - alpha)));
+                psi_n05(i - 1) = (q(i-1) + qe(g, i-1) + (2/(1 - alpha)) * (mu05 * psi_n05(i) / elem_length)) / (Et(g) + 2 * mu05 / (elem_length * (1 - alpha)));
+                psi_n07(i - 1) = (q(i-1) + qe(g, i-1) + (2/(1 - alpha)) * (mu07 * psi_n07(i) / elem_length)) / (Et(g) + 2 * mu07 / (elem_length * (1 - alpha)));
 
                 % compute the outgoing face flux
                 psi_n02(i - 2) = (2 / (1 - alpha)) * psi_n02(i-1) - ((1+alpha)/(1-alpha)) * psi_n02(i);
@@ -92,9 +100,9 @@ for g = 1:G
                 i = i - 2;
             end
             
-            % update the scattering source, and add the external source
+            % update the scattering source
             q_new = zeros(1, length(psi02));
-            q_new = Es(g) .* (1/6) .* (psi02 + psi_n02 + psi05 + psi_n05 + psi07 + psi_n07) + 1.0.*coordinates;
+            q_new = Es(g) .* (1/6) .* (psi02 + psi_n02 + psi05 + psi_n05 + psi07 + psi_n07);
 
             % test for convergence
             norm = 0;
@@ -118,10 +126,6 @@ for g = 1:G
        
         % save the scalar flux to the appropriate energy group
         group_flux(g,:) = phi;
-        
-        % save the angular flux as well
-        group_flux02(g,:) = psi02(2:2:end);
-        %0.5 * (psi02(2:2:end) + psi_n02(2:2:end));
 end    
         
         
@@ -129,13 +133,12 @@ end
         
         
         
-                % plot the scalar flux
-        %subplot(3, 2, plot_num)
-%         plot(coordinates(2:2:end), phi, '*')
-%         hold on
-%         grid on
-%         legend(sprintf('h = %.2f', h))
-%         xlabel('Problem Domain', 'FontSize', fontsize-2)
-%         ylabel('Scalar Flux', 'FontSize', fontsize-2)
+% plot the scalar flux
+plot(coordinates(2:2:end), group_flux(1,:), '*', coordinates(2:2:end), group_flux(2,:), '*', coordinates(2:2:end), group_flux(3,:), '*')
+hold on
+grid on
+legend('Group 1', 'Group 2', 'Group 3')
+xlabel('Problem Domain', 'FontSize', fontsize-2)
+ylabel('Scalar Flux', 'FontSize', fontsize-2)
 %saveas(gcf, 'ScalarFlux_partD', 'jpeg')
 %close all
