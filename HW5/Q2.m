406740ec695dadcf5e9f1acdbc40000edc741953
aprilnovak <novak@berkeley.edu>
2016-11-17 14:51:39 -0800

    Added all three angles and began within-group solve with group-specific constants.

--- HW5/Q2.m
+++ HW5/Q2.m
@@ -5,53 +5,60 @@ clear all
 fontsize = 16;
 
 % material data
-mu = 0.2;
+mu02 = 0.2;
+mu05 = 0.5;
 mu07 = 0.7;
-Et = 1.0;
-Es = 0.9;
+Et = [0.5, 0.8, 1.0];
+Es = [0.1+0.3+0.1, 0.1+0.3, 0.1+0.3];
 tolerance = 0.01;
 norm = 1; % intial arbitrary value
 num_iterations = 0;
 max_iterations = 1000;
 plot_num = 1;
+alpha = 0.50;
+h = 0.10;
+mesh = 0:h:2;
+elem_length = mesh(2);
+coordinates = 0:(elem_length / 2):2;
 
-for alpha = [0.0]
+% initial guess for the scattering source for __each__ group. After each 
+% within-group calculation, this source will be specialized depending
+% on the energy group.
+q = 1.0 .* coordinates;
+g = 1; % energy group
 
-    for h = [0.08]
-        
-        mesh = 0:h:2;
-        elem_length = mesh(2);
-        coordinates = 0:(elem_length / 2):2;
+
+
+        % each within-group calculation finds the angular flux for that group
 
         % initial guess for the scattering source is simply set to the 
         % external source (equivalent to guessing psi = 0.0)
-        q = 1.0 .* coordinates;
 
         % iterate
         while ((norm > tolerance) && (num_iterations < max_iterations))
-            
-            if (elem_length > 2 * mu / Et)
-                sprintf('Warning: Possibly negative flux!')
-            end
 
             % initialize
-            psi = zeros(1, length(coordinates));
-            psi_n = zeros(1, length(coordinates));
+            psi02 = zeros(1, length(coordinates));
+            psi_n02 = zeros(1, length(coordinates));
+            psi05 = zeros(1, length(coordinates));
+            psi_n05 = zeros(1, length(coordinates));
             psi07 = zeros(1, length(coordinates));
             psi_n07 = zeros(1, length(coordinates));
 
-            psi(1) = 2.0; % incoming flux boundary condition
+            psi02(1) = 2.0; % incoming flux boundary condition
+            psi05(1) = 2.0; % incoming flux boundary condition
+            psi07(1) = 2.0; % incoming flux boundary condition
 
             % positive sweep
             i = 1;
 
             while i <= (length(coordinates) - 2)
                 % compute cell-centered value
-                psi(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu) * psi(i) ./ elem_length)) / (Et + (2 / (1 + alpha)) * abs(mu) / elem_length);
-                psi07(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu07) * psi07(i) ./ elem_length)) / (Et + (2 / (1 + alpha)) * abs(mu07) / elem_length);
+                psi02(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu02) * psi02(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu02) / elem_length);
+                psi07(i + 1) = (q(i+1) + (2 / (1 + alpha)) * (abs(mu07) * psi07(i) ./ elem_length)) / (Et(g) + (2 / (1 + alpha)) * abs(mu07) / elem_length);
 
                 % compute out-going value
-                psi(i + 2) = (2 / (1 + alpha)) * psi(i + 1) - ((1 - alpha)/(1 + alpha)) * psi(i);
+                psi02(i + 2) = (2 / (1 + alpha)) * psi02(i + 1) - ((1 - alpha)/(1 + alpha)) * psi02(i);
                 psi07(i + 2) = (2 / (1 + alpha)) * psi07(i + 1) - ((1 - alpha)/(1 + alpha)) * psi07(i);
    
                 % move to next cell
@@ -61,25 +68,28 @@ for alpha = [0.0]
 
             % negative sweep - store the outgoing flux to apply periodic BC
             i = length(coordinates);
-            psi_n(i) = psi(end);
+            psi_n02(i) = psi02(end);
+            psi_n05(i) = psi05(end);
             psi_n07(i) = psi07(end);
 
             while i > 2
 
                 % compute the cell-centered flux
-                psi_n(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu * psi_n(i) / elem_length)) / (Et + 2 * mu / (elem_length * (1 - alpha)));
-                psi_n07(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu07 * psi_n07(i) / elem_length)) / (Et + 2 * mu07 / (elem_length * (1 - alpha)));
+                psi_n02(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu02 * psi_n02(i) / elem_length)) / (Et(g) + 2 * mu02 / (elem_length * (1 - alpha)));
+                psi_n05(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu05 * psi_n05(i) / elem_length)) / (Et(g) + 2 * mu05 / (elem_length * (1 - alpha)));
+                psi_n07(i - 1) = (q(i-1) + (2/(1 - alpha)) * (mu07 * psi_n07(i) / elem_length)) / (Et(g) + 2 * mu07 / (elem_length * (1 - alpha)));
 
                 % compute the outgoing face flux
-                psi_n(i - 2) = (2 / (1 - alpha)) * psi_n(i-1) - ((1+alpha)/(1-alpha)) * psi_n(i);
+                psi_n02(i - 2) = (2 / (1 - alpha)) * psi_n02(i-1) - ((1+alpha)/(1-alpha)) * psi_n02(i);
+                psi_n05(i - 2) = (2 / (1 - alpha)) * psi_n05(i-1) - ((1+alpha)/(1-alpha)) * psi_n05(i);
                 psi_n07(i - 2) = (2 / (1 - alpha)) * psi_n07(i-1) - ((1+alpha)/(1-alpha)) * psi_n07(i);
 
                 i = i - 2;
             end
             
             % update the scattering source, and add the external source
-            q_new = zeros(1, length(psi));
-            q_new = Es .* 0.5 .* (psi + psi_n + psi07 + psi_n07) + 1.0.*coordinates;
+            q_new = zeros(1, length(psi02));
+            q_new = Es(g) .* (1/6) .* (psi02 + psi_n02 + psi05 + psi_n05 + psi07 + psi_n07) + 1.0.*coordinates;
 
             % test for convergence
             norm = 0;
@@ -98,18 +108,27 @@ for alpha = [0.0]
         end
         
         % compute scalar flux
-        phi = 0.5 .* (psi(2:2:end) + psi_n(2:2:end) + psi07(2:2:end) + psi_n07(2:2:end));
+        phi = (1/6) .* (psi02(2:2:end) + psi_n02(2:2:end) + psi05(2:2:end) + psi_n05(2:2:end) + psi07(2:2:end) + psi_n07(2:2:end));
+       
+        % save the scalar flux to the appropriate energy group
+        group_flux(g,:) = phi;
         
-        % plot the scalar flux
+        % perform the next energy group
+        g = g + 1;
+        
+        
+        
+        
+        
+        
+        
+                % plot the scalar flux
         %subplot(3, 2, plot_num)
-        plot(coordinates(2:2:end), phi, '*')
-        hold on
-        grid on
-        legend(sprintf('h = %.2f', h))
-        xlabel('Problem Domain', 'FontSize', fontsize-2)
-        ylabel('Scalar Flux', 'FontSize', fontsize-2)
-    end 
-end
-
-saveas(gcf, 'ScalarFlux_partD', 'jpeg')
+%         plot(coordinates(2:2:end), phi, '*')
+%         hold on
+%         grid on
+%         legend(sprintf('h = %.2f', h))
+%         xlabel('Problem Domain', 'FontSize', fontsize-2)
+%         ylabel('Scalar Flux', 'FontSize', fontsize-2)
+%saveas(gcf, 'ScalarFlux_partD', 'jpeg')
 %close all
